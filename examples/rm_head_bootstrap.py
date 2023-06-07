97c08d72080803fca4b794eefcdd54d2d371d24d
Winnie Chow <wc459@cornell.edu>
2023-06-07 02:41:59 +0000

    Running head bootstrap

---
+++ examples/rm_head_bootstrap.py
@@ -0,0 +1,233 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Union
+import os
+import sys
+sys.path.remove(os.path.abspath(os.path.dirname(sys.argv[0])))
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from datasets import load_dataset
+# from peft import LoraConfig, TaskType, get_peft_model
+
+from transformers.modeling_utils import SequenceSummary
+
+
+from transformers import (
+    AutoModel,
+    AutoTokenizer,
+    HfArgumentParser,
+    Trainer,
+    PretrainedConfig
+)
+from lmflow.args import (
+    ModelArguments,
+    DatasetArguments,
+    AutoArguments,
+)
+
+from transformers import AdamW, get_linear_schedule_with_warmup
+from tqdm import tqdm
+
+k = 1 #TODO: remove hard-coding
+
+## Prepare training_args
+pipeline_name = "finetuner"
+PipelineArguments = AutoArguments.get_pipeline_args_class(pipeline_name)
+parser = HfArgumentParser((ModelArguments, DatasetArguments, PipelineArguments))
+
+if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+    model_args, data_args, pipeline_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+else:
+    model_args, data_args, pipeline_args = parser.parse_args_into_dataclasses()
+
+pipeline_args.remove_unused_columns = False 
+pipeline_args.label_names = []
+
+## Get model, by default we use lora to accelerate training
+# peft_config = LoraConfig(
+#     task_type=TaskType.SEQ_CLS,
+#     r=16,
+#     lora_alpha=32,
+#     lora_dropout=0.1,
+# )
+# Load in model without head
+model = AutoModel.from_pretrained(model_args.model_name_or_path, num_labels=1, torch_dtype=torch.bfloat16)
+
+# model_lora = get_peft_model(model, peft_config)
+# model_lora.print_trainable_parameters()
+
+## Get tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
+
+if "llama" in model_args.model_name_or_path:
+    tokenizer.add_special_tokens(
+        {
+            "eos_token": "[PAD]",
+            "bos_token": "</s>",
+            "unk_token": "</s>",
+            "pad_token": "</s>",
+        }
+    )
+else:
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+
+# We also need to add a pad_token for the model. Otherwise, the reward model cannot handle a batch of inputs
+model.config.pad_token_id = tokenizer.eos_token_id
+assert model.config.pad_token_id == tokenizer.pad_token_id
+
+## Get the dataset
+#TODO: change this to bootstrapping
+def build_dataset(tokenizer, config):
+    ''' 
+    Return whole dataset without validation split
+    We assume that we have preprocessed the dataset appropriately such that the sample is organized as follows:
+    {"positive": prompt + answer_positive, "negative": prompt + answer_negative}, where the positive response is preferred.
+    '''
+    def tokenize(sample):
+        tokenized_pos = tokenizer(sample['positive'], truncation=True)
+        tokenized_neg = tokenizer(sample['negative'], truncation=True)
+        sample["chosen_input_ids"] = tokenized_pos["input_ids"]
+        sample["chosen_attention_mask"] = tokenized_pos["attention_mask"]
+        sample["rejected_input_ids"] = tokenized_neg["input_ids"]
+        sample["rejected_attention_mask"] = tokenized_neg["attention_mask"]
+        return sample
+
+    ds = load_dataset("json", data_files=config.dataset_path, split="train", field="instances")
+    ds = ds.map(tokenize, batched=False)
+    ds = ds.filter(lambda x: len(x["chosen_input_ids"]) <= 512 and len(x["rejected_input_ids"]) <= 512)
+
+    return ds
+
+## Define the trainer
+def compute_metrics(eval_pred):
+    result = {}
+    pos_predictions_scores = eval_pred.predictions[0]
+    neg_predictions_scores = eval_pred.predictions[1]
+    # We assume that the first sample is preferred by default in groundtruth
+    result['accuracy'] = np.sum(pos_predictions_scores >= neg_predictions_scores) / len(pos_predictions_scores)
+    return result
+    
+class DataCollatorReward:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+
+    def __call__(self, data):
+        batch = {}
+        data_pos = []
+        data_neg = []
+        for sample in data:
+            data_pos.append({"input_ids": sample['chosen_input_ids'], "attention_mask": sample["chosen_attention_mask"]})
+            data_neg.append({"input_ids": sample['rejected_input_ids'], "attention_mask": sample["rejected_attention_mask"]})
+        batch_pos = self.tokenizer.pad(data_pos, padding=True, return_tensors="pt")
+        batch_neg = self.tokenizer.pad(data_neg, padding=True, return_tensors="pt")
+        batch['chosen_input_ids'] = batch_pos['input_ids']
+        batch['rejected_input_ids'] = batch_neg['input_ids']
+        batch['chosen_attention_mask'] = batch_pos['attention_mask']
+        batch['rejected_attention_mask'] = batch_neg['attention_mask']
+        batch['return_loss'] = True
+        return batch
+
+
+# class RMTrainer(Trainer):
+
+
+data_collator = DataCollatorReward(tokenizer=tokenizer)
+
+ds = build_dataset(tokenizer, data_args)
+eval_dataset = None
+
+if data_args.validation_split_percentage > 0:
+    idx_gap = int((1-data_args.validation_split_percentage/100) * len(ds))
+    train_dataset = ds.select(range(idx_gap))
+    eval_dataset = ds.select(range(idx_gap, len(ds)))
+else:
+    train_dataset = ds
+
+print("Training set: ", len(train_dataset), " Eval set: ", len(eval_dataset))
+if not eval_dataset and pipeline_args.eval_steps > 0:
+    raise valueerror("Cannot evaluate on an empty eval set")
+
+
+def compute_loss(pretrained, value_head, inputs, return_outputs=False):
+    chosen_hidden = pretrained(input_ids=inputs["chosen_input_ids"], attention_mask=inputs["chosen_attention_mask"])[0]
+    rejected_hidden = pretrained(input_ids=inputs["rejected_input_ids"], attention_mask=inputs["rejected_attention_mask"])[0]
+    chosen_rewards = value_head(chosen_hidden, None).squeeze(-1)
+    rejected_rewards = value_head(rejected_hidden, None).squeeze(-1)
+    loss = -nn.functional.logsigmoid(chosen_rewards - rejected_rewards).mean()
+    if return_outputs:
+        return loss, {"chosen_rewards": chosen_rewards, "rejected_rewards": rejected_rewards}
+    return loss
+
+def train(pretrained, train_data, eval_data):
+    
+    config = PretrainedConfig.from_pretrained("output_models/finetune-gpt-neo/config.json") # TODO: remove hard code
+    value_head = SequenceSummary(config)
+    # print(value_head)
+
+    train_dataloader = DataLoader(train_data, batch_size=16, collate_fn=data_collator)
+    eval_dataloader = DataLoader(eval_data, batch_size=16, collate_fn=data_collator)
+
+    num_epochs = 1
+    num_training_steps = 3 * len(train_dataloader)
+    optimizer = AdamW(list(value_head.parameters()), lr=5e-5, weight_decay=0.01)
+    lr_scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
+
+    best_val_loss = float("inf")
+    # progress_bar = tqdm(range(num_training_steps))
+
+    with tqdm(range(num_training_steps)) as progress_bar:
+        for epoch in range(num_epochs):
+            # training
+            value_head.train()
+            for batch_i, batch in enumerate(train_dataloader):
+                # batch = ([text1, text2], [0, 1])
+                optimizer.zero_grad()
+                loss = compute_loss(pretrained, value_head, batch, return_outputs=False)
+                loss.backward()
+                optimizer.step()
+                lr_scheduler.step()
+                progress_bar.update(1)
+            
+            # validation
+            value_head.eval()
+            for batch_i, batch in enumerate(eval_dataloader):
+                with torch.no_grad():
+                    loss += compute_loss(pretrained, value_head, batch, return_outputs=False)
+            
+            avg_val_loss = loss / len(eval_dataloader)
+            print(f"Validation loss: {avg_val_loss}")
+            if avg_val_loss < best_val_loss:
+                print("Saving checkpoint!")
+                best_val_loss = avg_val_loss
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': value_head.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'val_loss': best_val_loss,
+                    },
+                    f"checkpoints/epoch_{epoch}.pt"
+                )  
+
+# Instantiate model without head to prevent multiple copies
+pretrained = AutoModel.from_pretrained("output_models/finetune-gpt-neo")
+# # Freeze all layers
+# for param in pretrained.parameters():
+#     param.requires_grad = False
+    
+for i in range(k):
+    
+    # Subsample from train and eval datasets separately
+    idx_train = np.random.randint(len(train_dataset), size=len(train_dataset))
+    train_sub = train_dataset.select(idx_train)
+    idx_eval = np.random.randint(len(eval_dataset), size=len(eval_dataset))
+    eval_sub = eval_dataset.select(idx_eval)
+
+    train(pretrained, train_sub, eval_sub)
+    ## Save model
+    # model_lora.save_pretrained(f"{pipeline_args.output_dir}/head_{i}")
+
+
+
+
---
+++ scripts/run_rm_head_bootstrap.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Please run this script under ${project_id} in project directory of
+#   https://github.com/shizhediao/llm-ft
+#     COMMIT: d5fecf30ba8011067b10cf51fede53a5ab6574e4
+
+deepspeed_args="--master_port=11000"      # Default argument
+if [ $# -ge 1 ]; then
+  deepspeed_args="$1"
+fi
+
+project_dir=$(cd "$(dirname $0)"/..; pwd)
+dataset_path=${project_dir}/data/anthropic/harmless-base/rm/train/dataset.json
+model_name=finetune-gpt-neo
+model_path=output_models/${model_name}
+# model_name=gpt2
+# model_path=gpt2
+k=1
+exp_id=rm_head_${model_name}_bs_${k}
+output_dir=${project_dir}/output_models/${exp_id}
+log_dir=${project_dir}/log/${exp_id}
+
+mkdir -p ${output_dir} ${log_dir}
+
+CUDA_VISIBLE_DEVICES=2,3 \
+  deepspeed ${deepspeed_args} \
+    examples/rm_head_bootstrap.py \
+      --model_name_or_path ${model_path} \
+      --dataset_path ${dataset_path} \
+      --output_dir ${output_dir} --overwrite_output_dir \
+      --num_train_epochs 1 \
+      --learning_rate 3e-5 \
+      --block_size 512 \
+      --per_device_train_batch_size 2 \
+      --per_device_eval_batch_size 2 \
+      --gradient_accumulation_steps 16 \
+      --deepspeed configs/ds_config_zero2.json \
+      --bf16 \
+      --run_name rm_${k}_heads \
+      --validation_split_percentage 10 \
+      --logging_steps 10 \
+      --do_train \
+      --ddp_timeout 72000 \
+      --save_steps 999999 \
+      --evaluation_strategy steps\
+      --eval_steps 100\
+      --weight_decay 0.001\
+      --dataloader_num_workers 1 \
+      | tee ${log_dir}/train.log \
+      2> ${log_dir}/train.err
